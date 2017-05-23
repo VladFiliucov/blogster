2739977644116ae2aa6d2d57f8a1ef86aded4761
VladFiliucov <filiucov@mail.ru>
2017-05-23 20:41:56 +0100

    Send props from container component.

--- components/containers/BlogList.js
+++ components/containers/BlogList.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 import BlogItem from '../ui/BlogItem';
 
+const style = {width: 200, height: 200}
+const imageURL = "https://s-media-cache-ak0.pinimg.com/736x/8b/d1/b8/8bd1b8b02e9cb633adb1d789dd4588c6.jpg"
+const altText= "Guy rides a co...grown chicken"
+const text = "Lorem ipsum could be here"
+
 class BlogList extends React.Component {
   constructor(props) {
     super(props);
@@ -10,9 +15,9 @@ class BlogList extends React.Component {
   render() {
     return (
       <div>
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
+        <BlogItem imageURL={imageURL} style={style} altText={altText} text={text} />
+        <BlogItem imageURL={imageURL} style={style} altText={altText} text={text} />
+        <BlogItem imageURL={imageURL} style={style} altText={altText} text={text} />
       </div>
     );
   }
--- components/ui/BlogItem.jsx
+++ components/ui/BlogItem.jsx
@@ -3,19 +3,14 @@ import React from 'react';
 import Image from './Image';
 import TextBox from './TextBox';
 
-const style = {width: 200, height: 200}
-const imageURL = "https://s-media-cache-ak0.pinimg.com/736x/8b/d1/b8/8bd1b8b02e9cb633adb1d789dd4588c6.jpg"
-const altText= "Guy rides a co...grown chicken"
-const text = "Lorem ipsum could be here"
-
-const BlogItem = () => (
+const BlogItem = (props) => (
   <div>
     <Image
-      imageAddress={imageURL}
-      style={style}
-      altText={altText}
+      imageAddress={props.imageURL}
+      style={props.style}
+      altText={props.altText}
     />
-    <TextBox text={text} />
+    <TextBox text={props.text} />
   </div>
 )
 
