c5518d1f5781445769eafc63b13c5bb0e4aecb11
VladFiliucov <filiucov@mail.ru>
2017-05-26 23:52:14 +0100

    Add default props for Text Box.

--- components/ui/TextBox.jsx
+++ components/ui/TextBox.jsx
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types';
 
 const TextBox = ({children}) => <span> { children } </span>;
 
+TextBox.defaultProps = {
+  children: "Author didn't bother to provide text for this."
+}
+
 TextBox.propTypes = {
   children: PropTypes.string.isRequired
 }
