054f08076ef43f7f09e613380c87f42bc353df0d
VladFiliucov <filiucov@mail.ru>
2017-05-26 22:14:20 +0100

    Render meta data from details object.

--- components/ui/PostDetails.jsx
+++ components/ui/PostDetails.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const PostDetails = ({details}) => (
-  <h1>
-    {details.author}
-  </h1>
+  <div>
+    <h3>Author: {details.author}</h3>
+    <h3>Created At: {details.created_at}</h3>
+    <h3>Last Update: {details.updated_at}</h3>
+  </div>
 );
 
 export default PostDetails;
