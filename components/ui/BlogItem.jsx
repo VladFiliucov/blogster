614cd31bc35205f42ed1227ddee651d355eb58a1
VladFiliucov <filiucov@mail.ru>
2017-05-26 23:01:31 +0100

    Add PropTypes for BlogItem.

--- components/ui/BlogItem.jsx
+++ components/ui/BlogItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Image from './Image';
 import TextBox from './TextBox';
@@ -13,5 +14,26 @@ const BlogItem = (props) => (
     <Like likes={props.post.details.likes} />
   </div>
 )
+
+BlogItem.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.number,
+    text: PropTypes.string,
+    details: PropTypes.shape({
+      author: PropTypes.string,
+      created_at: PropTypes.string,
+      updated_at: PropTypes.string,
+      likes: PropTypes.number
+    }),
+    image: PropTypes.shape({
+      src: PropTypes.string,
+      alt: PropTypes.string,
+      style: PropTypes.shape({
+        width: PropTypes.number,
+        height: PropTypes.number
+      })
+    })
+  })
+}
 
 export default BlogItem;
