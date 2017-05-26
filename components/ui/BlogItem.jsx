552486c0cae680c41436db9ab7be5f21ea945d88
VladFiliucov <filiucov@mail.ru>
2017-05-26 22:11:50 +0100

    Add PostDetails component.

--- components/ui/BlogItem.jsx
+++ components/ui/BlogItem.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Image from './Image';
 import TextBox from './TextBox';
+import PostDetails from './PostDetails';
 
 const BlogItem = (props) => (
   <div>
     <Image image={props.post.image} />
     <TextBox>{props.post.text}</TextBox>
+    <PostDetails details={props.post.details} />
   </div>
 )
 
---
+++ components/ui/PostDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PostDetails = ({details}) => (
+  <h1>
+    {details.author}
+  </h1>
+);
+
+export default PostDetails;
+
