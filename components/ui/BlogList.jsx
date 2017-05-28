1bd2dea036495ed30b8d57f7940b58a33ddaf56e
VladFiliucov <filiucov@mail.ru>
2017-05-28 19:53:35 +0100

    Remove explicit return.

--- components/ui/BlogList.jsx
+++ components/ui/BlogList.jsx
@@ -11,12 +11,10 @@ export default class BlogList extends React.Component {
     return (
       <div>
         {
-          this.props.posts.map(post => {
-            return <BlogItem
-              key={post.id}
-              post={post}
-              incrementLikesForPost={this.props.incrementLikesForPost} />
-          })
+          this.props.posts.map(post => <BlogItem
+            key={post.id}
+            post={post}
+            incrementLikesForPost={this.props.incrementLikesForPost} />)
         }
       </div>
     );
