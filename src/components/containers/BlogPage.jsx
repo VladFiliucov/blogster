50d86f4fd16d587310b40c30af3edae3aec1430b
VladFiliucov <filiucov@mail.ru>
2017-05-30 21:44:43 +0100

    Fix linting.

--- src/components/containers/BlogPage.jsx
+++ src/components/containers/BlogPage.jsx
@@ -11,34 +11,38 @@ export default class BlogPage extends Component {
     super(props);
     this.state = {
       posts
-    }
-    this.incrementLikes = this.incrementLikes.bind(this)
+    };
+    this.incrementLikes = this.incrementLikes.bind(this);
   }
 
   incrementLikes(postId) {
     const { posts } = this.state;
-    const postIndex = posts.findIndex(post => post.id == postId)
+    const postIndex = posts.findIndex(post => post.id == postId);
 
     if (postIndex > -1) {
-      const post = posts[postIndex]
+      const post = posts[postIndex];
       const updatedPost = update(post, {
-        details: {likes: {$set: {post.details.likes + 1}}}
-      })
+        details: {likes: {$set: (post.details.likes + 1)}}
+      });
 
       this.setState({
         posts: posts
           .slice(0, postIndex)
           .concat(updatedPost)
           .concat(posts.slice(postIndex + 1))
-      })
+      });
     }
   }
 
   render() {
     return (
       <div>
-        <BlogList posts={this.state.posts} incrementLikes={this.incrementLikes} />
-        <PieChart columns={[ ...this.state.posts.map(post => [post.text, post.details.likes]) ]} />
+        <BlogList posts={this.state.posts}
+          incrementLikes={this.incrementLikes} />
+        <PieChart
+          columns={[ ...this.state.posts
+            .map(post => [post.text, post.details.likes]) ]}
+        />
       </div>
     );
   }
