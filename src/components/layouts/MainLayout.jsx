97bdafa568f56fa078724a8426a7ce73bb316e15
VladFiliucov <filiucov@mail.ru>
2017-06-03 21:32:49 +0100

    MainLayout layout.

---
+++ src/components/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React, { PropTypes } from 'react';
+
+const MainLayout = ({children}) => (
+  <div>
+    {children}
+  </div>
+);
+
+MainLayout.propTypes = {
+  children: PropTypes.node
+};
+
+export default MainLayout;
+
