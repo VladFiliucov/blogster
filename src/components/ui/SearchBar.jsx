63dd278d1bc4a4edd905ce4545f145e5c8a1a5ba
VladFiliucov <filiucov@mail.ru>
2017-06-05 09:38:50 +0100

    Add callback function on searchbar submit.

--- src/components/ui/SearchBar.jsx
+++ src/components/ui/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class SearchBar extends Component {
   constructor(props) {
@@ -12,12 +13,24 @@ export default class SearchBar extends Component {
     this.setState({ searchInput: e.target.value });
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.searchPosts(this.state.searchInput);
+  }
+
   render() {
     return (
-      <input type="text"
-        value={this.state.searchInput}
-        onChange={this.updateInput.bind(this)} />
+      <form onSubmit={this.handleSubmit}>
+        <input type="text"
+          value={this.state.searchInput}
+          onChange={this.updateInput.bind(this)} />
+        <button type="submit">Search</button>
+      </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchPosts: PropTypes.func
+};
 
