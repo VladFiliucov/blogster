4430967e3a60e8aa08dc1c157c0171c25abfd1e2
VladFiliucov <filiucov@mail.ru>
2017-06-09 23:54:48 +0100

    Style navigation NavLink's

--- src/components/ui/shared/Navigation.jsx
+++ src/components/ui/shared/Navigation.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import 'components/styles/Navigation.css';
+
 const Navigation = () => (
-  <nav>
-    <NavLink exact to='/' >Home</NavLink>
-    <NavLink exact to='/about' >About</NavLink>
+  <nav className='navigation-menu-container'>
+    <NavLink exact to='/'
+      className='nav-link'
+      activeStyle={{ background: '#80D8FF' }} >
+      <div>
+        Home
+      </div>
+    </NavLink>
+    <NavLink exact to='/about'
+      className='nav-link'
+      activeStyle={{ background: '#80D8FF' }} >
+      <div>
+        About
+      </div>
+    </NavLink>
   </nav>
 );
 
