fc75da46644c55c2b8756cf80e861e3c56562c80
VladFiliucov <filiucov@mail.ru>
2017-06-09 22:36:52 +0100

    Add Material search input.

--- src/components/ui/SearchBar.jsx
+++ src/components/ui/SearchBar.jsx
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import TextField from 'material-ui/TextField';
+
 export default class SearchBar extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchTerm: ''
+    };
+    this.updateSearchTerm = this.updateSearchTerm.bind(this);
+  }
+
+  updateSearchTerm(e) {
+    this.setState({ searchTerm: e.target.value });
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    this.props.searchPosts(this.refs.search.value, e);
-    this.refs.search.value = '';
+    this.props.searchPosts(this.state.searchTerm, e);
   }
 
   render() {
     return (
       <div>
-        <form onSubmit={ this.handleSubmit.bind(this) }>
-          <input type="text" ref='search' />
+        <form onSubmit={ this.handleSubmit.bind(this) } >
+          <TextField
+            hintText="What do you want to find?"
+            floatingLabelText="Search by title"
+            value={ this.state.searchTerm }
+            onChange={ this.updateSearchTerm }
+          />
           <button type="submit">Search</button>
         </form>
       </div>
