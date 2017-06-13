8010334ea0c59950fb0f11f5dad8784d8e8f2f15
VladFiliucov <filiucov@mail.ru>
2017-06-14 00:40:36 +0100

    Add store to App.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import ApplicationRouter from 'routes';
-
+import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import store from 'store';
+import ApplicationRouter from 'routes';
 
 class App extends React.Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <ApplicationRouter />
-      </MuiThemeProvider>
+      <Provider store={store}>
+        <MuiThemeProvider>
+          <ApplicationRouter />
+        </MuiThemeProvider>
+      </Provider>
     );
   }
 }
