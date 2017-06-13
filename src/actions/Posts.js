6b3e7bc19838e4e026e389fa1234e0c96308d8f9
VladFiliucov <filiucov@mail.ru>
2017-06-13 22:34:15 +0100

    Implement Posts actions.

---
+++ src/actions/Posts.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+import { SERVER_ENDPOINT } from 'constants/static/env';
+import { postsPath } from 'helpers/routes/posts.js';
+
+import * as types from 'constants/actionTypes/PostsActionTypes';
+
+const requestPosts = () => ({
+  type: types.FETCH_POSTS_REQUEST
+});
+
+const recievePosts = (response) => ({
+  type: types.FETCH_POSTS_SUCCESS,
+  response
+});
+
+const errorPosts = () => ({
+  type: types.FETCH_POSTS_ERROR
+});
+
+export function fetchPosts() {
+  const querryObject = { BASE_URL: SERVER_ENDPOINT };
+
+  return (dispatch) => {
+    dispatch(requestPosts());
+
+    return axios
+    .get(postsPath(querryObject))
+      .then(response => {
+        dispatch(recievePosts(response));
+      })
+      .catch(error => {
+        dispatch(errorPosts());
+      });
+  };
+}
+
