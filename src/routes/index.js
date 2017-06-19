c8dd6ee1c7ba72b22238edcc9d74366a1e68bc39
VladFiliucov <filiucov@mail.ru>
2017-06-19 17:39:53 +0100

    Export Blog by default from root routes file.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
+import { Route } from 'react-router-dom';
+
+import Blog from 'routes/Blog';
 
 import BlogPage from 'components/containers/BlogPage';
 import BlogShow from 'components/containers/BlogShow';
@@ -10,28 +9,30 @@ import MainLayout from 'components/layouts/MainLayout';
 import Navigation from 'components/ui/shared/Navigation';
 import About from 'components/ui/shared/About';
 
+import { fetchPosts } from 'actions/Posts';
+import { fetchPost } from 'actions/Post';
+
 class ApplicationRouter extends Component {
   render() {
     return (
-      <Router>
-        <MainLayout>
-          <Navigation />
-          <div>
-            <Route exact path="/" render={({match, location, history}) => (
-              <BlogPage match={match}
-                location={location}
-                history={history} />
-            ) } />
-            <Route exact path="/about" component={ About } />
-            <Route exact path="/posts/:postId" render={({match}) => (
-              <BlogShow postId={match.params.postId} />
-            )} />
-          </div>
-        </MainLayout>
-      </Router>
+      <MainLayout>
+        <Navigation />
+        <div>
+          <Route exact path="/" render={({match, location, history}) => (
+            <BlogPage match={match}
+              location={location}
+              history={history}
+               />
+          ) } />
+          <Route exact path="/about" component={ About } />
+          <Route exact path="/posts/:postId" render={({match}) => (
+            <BlogShow postId={match.params.postId} />
+          )} />
+        </div>
+      </MainLayout>
     );
   }
 }
 
-export default ApplicationRouter;
+export default [Blog]
 
