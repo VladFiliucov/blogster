1bcaf56ebbafe00d330ea59b50b870e4d52bba48
VladFiliucov <filiucov@mail.ru>
2017-06-24 11:57:13 +0100

    prepareData func for BlogRoute. Edit fetch post id in render().

--- src/routes/Blog.js
+++ src/routes/Blog.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import BlogShow from 'components/containers/BlogShow';
+import { fetchPost } from 'actions/Post';
 
 const BlogRoute = {
   exact: true,
-  path: '/posts/:postId',
-  render: ({match}) => (
-    <BlogShow postId={match.params.postId} />
+  path: '/posts',
+  prepareData: (store) => (
+    store.dispatch(
+      fetchPost(new URLSearchParams(location.search).get('postId')))
+  ),
+  render: ({ location }) => (
+    <BlogShow postId={new URLSearchParams(location.search).get('postId')} />
   )
+};
+
+BlogRoute.propTypes = {
+  location: PropTypes.object.isRequired,
 };
 
 export default BlogRoute;
