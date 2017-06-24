fe15d7f98ead0cadcf206b768a82444a8dac7595
VladFiliucov <filiucov@mail.ru>
2017-06-24 11:55:58 +0100

    Add prepareData func to BlogPage route.

--- src/routes/BlogList.js
+++ src/routes/BlogList.js
@@ -1,9 +1,13 @@
 import BlogPage from 'components/containers/BlogPage';
+import { fetchPosts } from 'actions/Posts';
 
 const BlogsRoute = {
   exact: true,
   path: '/',
-  component: BlogPage
+  component: BlogPage,
+  prepareData: (store) => (
+    store.dispatch(fetchPosts())
+  )
 };
 
 export default BlogsRoute;
