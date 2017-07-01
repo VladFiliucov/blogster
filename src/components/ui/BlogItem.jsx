52c76a69a871f50118f2bbe7fcbbc09aea789038
VladFiliucov <filiucov@mail.ru>
2017-07-01 17:28:08 +0100

    Create path helper function for post Link.

--- src/components/ui/BlogItem.jsx
+++ src/components/ui/BlogItem.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+import { postUrlObject } from 'helpers/routes/post';
+
 import Image from './Image';
 import TextBox from './TextBox';
 import PostDetails from './PostDetails';
@@ -11,7 +13,7 @@ import 'components/styles/blog/BlogItem.css'
 
 const BlogItem = ({ post }) => (
   <div className="wrapper card radius shadowDepth1">
-    <Link  to={{pathname: '/posts', search: `postId=${post.id}`}} >
+    <Link  to={postUrlObject(post.id)} >
       <Image image={post.image} />
     </Link>
     <div className="card__content card__padding">
--- src/helpers/routes/post.js
+++ src/helpers/routes/post.js
@@ -2,4 +2,11 @@ const postPath = ({ BASE_URL, id }) => {
   return `${BASE_URL}/posts/${id}`;
 };
 
-export { postPath };
+const postUrlObject = (postId) => (
+  {
+    pathname: '/posts',
+    search: `postId=${postId}`
+  }
+);
+
+export { postPath, postUrlObject };
