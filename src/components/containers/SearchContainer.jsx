5aef1a61850ff919b0fe15796433c5d6672d8a33
VladFiliucov <filiucov@mail.ru>
2017-07-16 17:08:04 +0300

    Dispatch push action with react-redux router.

--- src/components/containers/SearchContainer.jsx
+++ src/components/containers/SearchContainer.jsx
@@ -1,16 +1,17 @@
 import { connect } from 'react-redux';
 import { fetchPosts } from 'actions/Posts';
+import { withRouter } from 'react-router-dom';
 import history from 'routes/history';
+import { push } from 'react-router-redux';
 
 import SearchBar from 'components/ui/SearchBar';
 
 function mapDispatchToProps(dispatch) {
   return {
     fetchPosts: (searchTerm) => {
-      dispatch(fetchPosts(searchTerm));
-      history.push({pathname: '/', search: searchTerm});
+      dispatch(push({pathname: '/', search: searchTerm}));
     }
   };
 }
 
-export default connect(null, mapDispatchToProps)(SearchBar);
+export default withRouter(connect(null, mapDispatchToProps)(SearchBar));
--- src/routes/BlogList.js
+++ src/routes/BlogList.js
@@ -1,17 +1,21 @@
+import React from 'react';
 import BlogPage from 'components/containers/BlogPage';
 import { fetchPosts } from 'actions/Posts';
+import { push } from 'react-router-redux';
 
 const BlogsRoute = {
   exact: true,
   path: '/',
-  component: BlogPage,
   prepareData: (store, state) => {
     if (state.location.search) {
-      store.dispatch(fetchPosts(state.location.search))
+      store.dispatch(fetchPosts(state.location.search));
     } else {
-      store.dispatch(fetchPosts())
+      store.dispatch(fetchPosts());
     }
-  }
+  },
+  render: ({ location }) => (
+    <BlogPage searchTerm={ location.search.slice(1) } />
+  )
 };
 
 export default BlogsRoute;
