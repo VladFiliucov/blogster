6919606eeef7c5b65baeda3a2aac40277ab7753c
VladFiliucov <filiucov@mail.ru>
2017-07-20 13:41:56 +0300

    Use object assign. Merge props function.

--- src/components/containers/BlogPage.jsx
+++ src/components/containers/BlogPage.jsx
@@ -2,19 +2,24 @@ import { connect } from 'react-redux';
 import update from 'immutability-helper';
 
 import { postUrlObject } from 'helpers/routes/post';
+import { assign } from 'lodash';
 import BlogPage from 'components/ui/BlogPage';
 
 const mapStateToProps = (state) => ({
   posts: state.posts.posts
-    .map(post => update(post,
-      {
-        $merge: {
-          postUrl: postUrlObject(post.id)
-        }
-      }
-    )),
+    .map(post => assign(
+      {}, post, {
+        postUrl: postUrlObject(post.id)
+      })
+    ),
   isLoading: state.posts.isLoading,
   error: state.posts.error
 });
 
-export default connect(mapStateToProps)(BlogPage);
+const mergeProps = (stateProps, dispatchProps, ownProps) => {
+  return ownProps.searchTerm.q
+    ? Object.assign({}, ownProps, { posts: stateProps.posts.filter(post => { return post.text.toLowerCase().indexOf(ownProps.searchTerm.q.toLowerCase()) > - 1; }) })
+    : Object.assign({}, ownProps, { posts: stateProps.posts })
+};
+
+export default connect(mapStateToProps, null, mergeProps)(BlogPage);
