6f430d9b270323dc9309f97bf87c4f9fd42c1c21
VladFiliucov <filiucov@mail.ru>
2017-07-20 13:37:54 +0300

    Parse searchTerm and pass it as own props.

--- src/routes/BlogList.js
+++ src/routes/BlogList.js
@@ -2,19 +2,24 @@ import React from 'react';
 import BlogPage from 'components/containers/BlogPage';
 import { fetchPosts } from 'actions/Posts';
 import { push } from 'react-router-redux';
+import { parse } from 'qs';
 
 const BlogsRoute = {
   exact: true,
   path: '/',
   prepareData: (store, state) => {
     if (state.location.search) {
-      store.dispatch(fetchPosts(state.location.search));
+      store.dispatch(
+        fetchPosts(new URLSearchParams(state.location.search)
+          .get('q')
+        )
+      );
     } else {
       store.dispatch(fetchPosts());
     }
   },
   render: ({ location }) => (
-    <BlogPage searchTerm={ location.search.slice(1) } />
+    <BlogPage searchTerm={ parse(location.search.slice(1)) } />
   )
 };
 
