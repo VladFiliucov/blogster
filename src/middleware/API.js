46636728582c60256f3798f1917b2d45467ff1c3
VladFiliucov <filiucov@mail.ru>
2017-07-25 21:15:40 +0300

    Add APICall method to hadle API calls.

---
+++ src/middleware/API.js
@@ -0,0 +1,22 @@
+import request from 'superagent';
+import { stringify } from 'qs';
+
+import { SERVER_ENDPOINT } from 'constants/static/env';
+
+function APICall({ endpoint, method, query, payload }) {
+  return new Promise((resolve, reject) => {
+    let r = request[method.toLowerCase()](`${SERVER_ENDPOINT}${endpoint}`);
+
+    if (query)
+      r.query(stringify(query));
+
+    if (payload)
+      r = r.send(payload);
+
+    r.end((error, data) => (
+      error
+      ? reject(error)
+      : resolve(data.body)
+    ));
+  });
+}
