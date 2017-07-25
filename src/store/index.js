82e542c8d39f52af3912688bba9620d0f9013cb2
VladFiliucov <filiucov@mail.ru>
2017-07-26 00:23:45 +0300

    Remove router middleware and extra history singleton.

--- src/store/index.js
+++ src/store/index.js
@@ -2,17 +2,11 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from 'reducers';
 import APIMiddleware from 'middleware/API';
 import DevTools from 'components/containers/DevTools';
-import createHistory from 'history/createBrowserHistory';
-import { routerMiddleware } from 'react-router-redux';
-
-const history = createHistory();
-const middleware = routerMiddleware(history);
 
 const store = createStore(
   reducers,
   compose(
     applyMiddleware(APIMiddleware),
-    applyMiddleware(middleware),
     DevTools.instrument()
   )
 );
