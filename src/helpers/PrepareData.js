37a4337e8d62ee0ed62749f0f5acd573036ae89c
VladFiliucov <filiucov@mail.ru>
2017-08-07 18:08:56 +0300

    Comment out yet unsused query deffinition.

--- src/helpers/PrepareData.js
+++ src/helpers/PrepareData.js
@@ -6,7 +6,7 @@ import { map } from 'lodash/collection';
 export default function (store, state) {
   const { location, params, routes } = state;
 
-  const query = parse(location.search.substr(1));
+  // const query = parse(location.search.substr(1));
 
   const prepareDataFns = compact(map(routes, route => route.prepareData));
 
