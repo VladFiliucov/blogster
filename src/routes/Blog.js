9455127adf5a1df3d6d51079a068537408c1b5b9
VladFiliucov <filiucov@mail.ru>
2017-08-14 09:28:23 +0300

    Implement SSR in Blog route.

--- src/routes/Blog.js
+++ src/routes/Blog.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { parse } from 'qs';
+import initialLoad from 'helpers/initialLoad';
+
 import BlogShow from 'components/containers/BlogShow';
 import { fetchPost } from 'actions/Post';
+import { postUrlObject } from 'helpers/routes/post';
 
 const BlogRoute = {
   exact: true,
-  path: '/posts',
+  path: postUrlObject(),
   prepareData: (store, state) => {
+    if (initialLoad()) return;
+
     const { postId } = state.query;
 
-    if (postId) return store.dispatch(fetchPost(postId));
+    if (!!postId) return store.dispatch(fetchPost(postId));
   },
-  render: ({ location }) => (
-    // <BlogShow postId={new URLSearchParams(location.search).get('postId')} />
-    <BlogShow postId={1} />
-  )
+  render: ({ location }) => {
+    console.log("GOT HERE");
+    return <BlogShow postId={ parse(location.search.substr(1).postId) } />
+  }
 };
 
 BlogRoute.propTypes = {
