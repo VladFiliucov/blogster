2eb1b34400f123392d8634d244e344adbff8777b
VladFiliucov <filiucov@mail.ru>
2017-08-20 11:23:55 +0300

    Add contact page.

---
+++ src/routes/Contact.js
@@ -0,0 +1,10 @@
+import ContactPage from 'components/views/ContactPage';
+
+const ContactRoute = {
+  exact: true,
+  strict: true,
+  path: '/contact',
+  component: ContactPage
+};
+
+export default ContactRoute;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import BlogsRoute from 'routes/BlogList';
 import BlogRoute from 'routes/Blog';
 import AboutRoute from 'routes/About';
+import ContactRoute from 'routes/Contact';
 
 import { fetchPosts } from 'actions/Posts';
 import { fetchPost } from 'actions/Post';
 
 export default () => ([
   AboutRoute,
+  ContactRoute,
   BlogRoute,
   BlogsRoute
 ]);
