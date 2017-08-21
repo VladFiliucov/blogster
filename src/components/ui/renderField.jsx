c588e2d1f8d8d59e10829931e5b3a09748000833
VladFiliucov <filiucov@mail.ru>
2017-08-21 23:10:57 +0300

    Use material ui text field for text fields.

--- src/components/ui/renderField.jsx
+++ src/components/ui/renderField.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import TextField from 'material-ui/TextField'
+
 const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
   <div>
-    <label>{label}</label>
-    <input {...input} type={type} />
+    <TextField hintText={label}
+      floatingLabelText={label}
+      errorText={touched && error}
+      {...input}
+    />
   </div>
 );
 
