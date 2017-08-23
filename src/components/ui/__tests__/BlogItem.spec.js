18c50bfdb8e70047d15a27a7fb5ece21c96374f0
VladFiliucov <filiucov@mail.ru>
2017-08-23 19:14:19 +0300

    BlogItem renders text

--- src/components/ui/__tests__/BlogItem.spec.js
+++ src/components/ui/__tests__/BlogItem.spec.js
@@ -4,12 +4,27 @@ import { shallow } from 'enzyme';
 
 import BlogItem from '../BlogItem';
 import LikeContainer from 'components/containers/LikeContainer';
-import 'components/styles/blog/BlogItem.css'
+import TextBox from 'components/ui/TextBox';
 
 describe('BlogItem', () => {
   it('renders withour crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<BlogItem />, div);
+  });
+
+  describe('render', () => {
+    it('should render post text', () => {
+      const itemProps = {
+        text: 'Text for blogpost',
+        id: 4,
+        postUrl: 'localhost:9090/posts/4'
+      };
+
+      const item = shallow(<BlogItem post={itemProps} />);
+      const description = <TextBox>Text for blogpost</TextBox>;
+
+      expect(item.contains(description)).toEqual(true);
+    });
   });
 });
 
