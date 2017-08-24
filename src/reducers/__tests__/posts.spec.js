14fd05153d235642251e2b8179d4118dd2c1d61b
VladFiliucov <filiucov@mail.ru>
2017-08-24 10:09:49 +0300

    Specs for like post action in posts reducer.

--- src/reducers/__tests__/posts.spec.js
+++ src/reducers/__tests__/posts.spec.js
@@ -1,12 +1,19 @@
 import postsReducer from 'reducers/Posts';
 import * as types from 'constants/actionTypes/PostsActionTypes';
 
+import { posts } from 'testFactories/posts';
+
 const initialState = {
   isLoading: false,
   error: false,
   posts: []
 };
 
+const stateWithFetchedPosts = {
+  posts,
+  isLoading: false,
+  error: false
+};
 
 describe('posts reducer', () => {
   it('should return initial state', () => {
@@ -14,7 +21,6 @@ describe('posts reducer', () => {
   });
 
   it('changes state to loading when posts are fetchin', () => {
-
     const action = {
       type: types.FETCH_POSTS_REQUEST
     };
@@ -26,6 +32,53 @@ describe('posts reducer', () => {
     };
 
     const actual = postsReducer(initialState, action);
+    expect(actual).toEqual(expected);
+  });
+
+  it('returns error true if fetching posts failed', () => {
+    const action = {
+      type: types.FETCH_POSTS_ERROR
+    };
+
+    const expected = {
+      isLoading: false,
+      error: true,
+      posts: []
+    };
+
+    const actual = postsReducer(initialState, action);
+    expect(actual).toEqual(expected);
+  });
+
+  it('increments likes for given post', () => {
+    const action = {
+      postId: 1,
+      type: types.LIKE_POST
+    };
+
+    const expected = {
+      isLoading: false,
+      error: false,
+      posts: [
+        {
+          id: 1,
+          text: 'This is an awesome blogpost 1',
+          details: {
+            author: 'Avdi Grimm',
+            createdAt: 'Yesterday',
+            updatedAt: 'Tomorrow',
+            likes: 13
+          },
+          image: {
+            src: '/pictures/picture1.jpg',
+            alt: 'Who cares',
+            style: {width: 200, height: 200}
+          }
+        }
+      ]
+    };
+
+    const actual = postsReducer(stateWithFetchedPosts, action);
     expect(actual).toEqual(expected);
   });
 });
---
+++ src/testFactories/posts.js
@@ -0,0 +1,18 @@
+export const posts = [
+  {
+    id: 1,
+    text: 'This is an awesome blogpost 1',
+    details: {
+      author: 'Avdi Grimm',
+      createdAt: 'Yesterday',
+      updatedAt: 'Tomorrow',
+      likes: 12
+    },
+    image: {
+      src: '/pictures/picture1.jpg',
+      alt: 'Who cares',
+      style: {width: 200, height: 200}
+    }
+  }
+];
+
