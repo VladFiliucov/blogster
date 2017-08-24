d4a1956428bfd2f0406889ad0fe4589209a3d31f
VladFiliucov <filiucov@mail.ru>
2017-08-24 09:05:33 +0300

    type.FETCHING_POSTS spec.

--- src/reducers/__tests__/posts.spec.js
+++ src/reducers/__tests__/posts.spec.js
@@ -1,15 +1,32 @@
-import reducer from 'reducers/Posts';
+import postsReducer from 'reducers/Posts';
 import * as types from 'constants/actionTypes/PostsActionTypes';
+
+const initialState = {
+  isLoading: false,
+  error: false,
+  posts: []
+};
+
 
 describe('posts reducer', () => {
   it('should return initial state', () => {
-    expect(reducer(undefined, {})).toEqual(
-      {
-        isLoading: false,
-        error: false,
-        posts: []
-      }
-    );
+    expect(postsReducer(undefined, {})).toEqual(initialState);
+  });
+
+  it('changes state to loading when posts are fetchin', () => {
+
+    const action = {
+      type: types.FETCH_POSTS_REQUEST
+    };
+
+    const expected = {
+      isLoading: true,
+      error: false,
+      posts: []
+    };
+
+    const actual = postsReducer(initialState, action);
+    expect(actual).toEqual(expected);
   });
 });
 
