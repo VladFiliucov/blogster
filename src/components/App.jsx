ac1e07ed4856bda3334fb2906794df8c6ce33737
VladFiliucov <filiucov@mail.ru>
2017-08-27 15:29:35 +0300

    Use devtools and hot reload in development only.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -77,10 +77,11 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(
-  <DevTools store={store} />,
-  document.getElementById('devtools')
-);
+if (__DEVELOPMENT__)
+  ReactDOM.render(
+    <DevTools store={store} />,
+    document.getElementById('devtools')
+  );
 
 export default App;
 
