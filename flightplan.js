d01c3f62060bd4977eac3463cab2b673ccaae244
VladFiliucov <filiucov@mail.ru>
2017-08-28 19:13:57 +0300

    Deploy attempt.

--- flightplan.js
+++ flightplan.js
@@ -4,21 +4,19 @@ var tmpDir = 'blog-clinet-' + new Date().getTime();
 var user = 'deploy';
 
 plan.target('production', {
-  host: '...',
+  host: '138.68.178.32',
+  port: 5555,
   username: user,
   agent: process.env.SSH_AUTH_SOCK
 });
 
 plan.local(function(local) {
-  local.log('Running specs');
-  local.exec('npm run test');
   local.log('Copy files to remote host');
   var filesToCopy = local.exec('git ls-files', {silent: true});
   local.transfer(filesToCopy, '/tmp/' + tmpDir);
 });
 
 plan.remote(function(remote) {
-  remote.exec('nvm use default');
   remote.log('Moving folder to web root');
   remote.exec('cp -R /tmp/' + tmpDir + ' ~');
   remote.rm('-rf /tmp/' + tmpDir);
@@ -30,8 +28,8 @@ plan.remote(function(remote) {
   remote.exec('npm --prefix ~/' + tmpDir + ' run build');
 
   remote.log('Reload application');
-  remote.exec('ln -snf ~/' + tmpDir + ' ~/current');
-  remote.exec('(cd ~/current && pm2 restart pm2.config.js --env production)');
+  remote.exec('ln -snf ~/blogster' + tmpDir + ' ~/blogster/current');
+  remote.exec('(cd ~/blogster/current && pm2 start pm2.config.js --env production)');
 
   remote.log('Deploy process completed.');
 });
